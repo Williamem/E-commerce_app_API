d93039a007d40f6c654e0fa9cc47cac62f0d6c91
WilliamEm <william.emanuelsson@gmail.com>
2024-02-23 13:51:30 +0800

    add userValidator

--- backend/validators/userValidator.js
+++ backend/validators/userValidator.js
@@ -1,9 +1,23 @@
 const Joi = require('joi')
 
 const userValidator = Joi.object({
-    name: Joi.string().min(6).required(),
-    email: Joi.string().min(6).required().email(),
-    password: Joi.string().min(6).required()
+    email: Joi.string()
+        .min(6)
+        .required()
+        .email()
+        .messages({
+            'string.empty': 'Email is required',
+            'string.min': 'Email should have a minimum length of 6',
+            'any.required': 'Email is required'
+        }),
+    password: Joi.string()
+        .min(6)
+        .required()
+        .messages({
+            'string.empty': 'Password is required',
+            'string.min': 'Password should have a minimum length of 6',
+            'any.required': 'Password is required'
+        })
 });
 
 module.exports = userValidator;
