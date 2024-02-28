324a94f986e3914e34f663c834f133a3b15ddc03
WilliamEm <william.emanuelsson@gmail.com>
2024-02-28 16:56:22 +0800

    add create address

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,7 +1,8 @@
 const bcrypt = require('bcryptjs');
 const User = require('../models/User');
 const UserRole = require('../models/UserRole');
-const { userValidator } = require('../validators/validators')
+const Address = require('../models/Address')
+const { userValidator, addressValidator } = require('../validators/validators')
 
 exports.createUser = async (req, res) => {
   const validationResult = userValidator.validate(req.body); // Validate input using Joi
@@ -45,14 +46,32 @@ exports.logout = async (req, res) => {
   })
 }
 
-/* app.get('/logout', (req, res) => {
-  req.logout(() => {
-      res.send({ message: 'logged out' });
-  });
-}); */
+exports.createAddress = async (req, res) => {
+  console.log('User ID from request parameters:', req.params.userId);
+  const validationResult = addressValidator.validate(req.body);
+  if (validationResult.error) { 
+    return res.status(400).json({ error: validationResult.error.details[0].message });
+  }
+  try {
 
-/* app.post('/login', passport.authenticate('local'), (req, res) => {
-  console.dir()
-  res.status(200).send({ message: 'Log in successful' });
-}); */
+    // Get role_id for 'regular_user'
+    // Assuming role_name for regular users is 'regular_user'
+    const userId = parseInt(req.params.userId);
+    console.log('userId: ', userId)
+    if (!userId) {
+      return res.status(404).json({error: 'User not found'})
+    }
+
+    address = await Address.create({
+        ...req.body, 
+        user_id: userId 
+    });
+    
+    res.status(201).json(address); // Sending back just the newly created object
+  } catch (error) {
+      console.error(error);
+      res.status(500).json({message: 'Server error'});
+  }
+
+}
 
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,10 +3,14 @@ const router = express.Router();
 const userController = require('../controllers/userController');
 const passport = require('passport');
 require('../config/passport')(passport);
+const { isCurrentUser, isAdmin } = require('../middleware/authorization');
 
 router.post('/register', userController.createUser);
 router.post('/login', passport.authenticate('local'), userController.login);
 router.get('/logout', userController.logout);
+
+// profile routes
+router.post('/:userId/address', (req, res, next) => isCurrentUser(req.params.userId, req, res, next), userController.createAddress);
 
 module.exports = router;
 
