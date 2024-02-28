95730bbba88bd13b7a318dff9b2794a9ff4d2640
WilliamEm <william.emanuelsson@gmail.com>
2024-02-28 16:56:41 +0800

    cleanup

--- backend/tests/users.test.js
+++ backend/tests/users.test.js
@@ -1,7 +1,7 @@
 const User = require('../models/User');
 const chai = require("chai");
 const chaiHttp = require("chai-http");
-const server = require("../app"); // import your express app
+const server = require("../app");
 
 chai.should();
 chai.use(chaiHttp);
