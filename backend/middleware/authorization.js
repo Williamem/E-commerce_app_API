83407973c418d6cbcf5a572521775ae47cadd06b
WilliamEm <william.emanuelsson@gmail.com>
2024-02-29 15:58:34 +0800

    add isCurrentUserOrAdmin test

--- backend/middleware/authorization.js
+++ backend/middleware/authorization.js
@@ -8,16 +8,31 @@ const isAdmin = (req, res, next) => {
 
 // authorization.js
 const isCurrentUser = (idToAccess, req, res, next) => {
-    console.log('idToAccess: ', idToAccess);
-    console.log('req.user.datavalues.id ', req.user.dataValues.id)
-    if (req.user && parseInt(req.user.dataValues.id) === parseInt(idToAccess)) {
+    if (!req.user) {
+        return res.status(401).json({ error: 'User is not signed in' }); // Unauthorized: User is not signed in
+    }
+/*     console.log('idToAccess: ', idToAccess);
+    console.log('req.user.datavalues.id ', req.user.dataValues.id) */
+    if (parseInt(req.user.dataValues.id) === parseInt(idToAccess)) {
         next(); // User authorized, continue to the next middleware or route handler
     } else {
         res.status(403).json({ error: 'Unauthorized' }); // User is not authorized
     }
-}
+};
+
+const isCurrentUserOrAdmin = (idToAccess, req, res, next) => {
+    if (!req.user) {
+        return res.status(401).json({ error: 'User is not signed in' }); // Unauthorized: User is not signed in
+    }
+    if (req.user.dataValues.id === parseInt(idToAccess) || req.user.dataValues.role_id === 1) {
+        next(); // User is authorized, continue to the next middleware or route handler
+    } else {
+        res.status(403).json({ error: 'Unauthorized' }); // User is not authorized
+    }
+};
 
 module.exports = {
     isAdmin,
     isCurrentUser,
+    isCurrentUserOrAdmin,
 }
